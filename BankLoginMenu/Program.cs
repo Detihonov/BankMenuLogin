77bc57649c22ea50b71ef019e560886de7033be3
Denis_Tihonov <ndenmru@gmail.com>
2025-06-12 12:58:46 +0300

    Create project BankLoginMenu

---
+++ BankLoginMenu/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLoginMenu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******************** Bank ********************");
+            Console.WriteLine("::Logon Page::");
+
+            // declare variables to store username and password
+            string userName = null, password = null;
+
+            // read userName from keyboard
+            Console.Write("UserName: ");
+            userName = Console.ReadLine(); // John
+
+            if (userName != "")
+            {
+                // read password from keyboard
+                Console.Write("Password: ");
+                password = Console.ReadLine(); 
+            }
+
+            // chack username and password
+            if (userName.ToLower() == "system" && password == "manager")
+            {
+                Console.WriteLine("TO DO: Main menu here");
+            }
+
+            // about to exit
+            Console.WriteLine("Thank you! Visit again.");
+        }
+    }
+}

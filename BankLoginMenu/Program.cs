daffeb7399b2b6ffc2af326b07896781093100a5
Denis_Tihonov <ndenmru@gmail.com>
2025-06-12 13:24:53 +0300

    Add Switch Main Menu

--- BankLoginMenu/Program.cs
+++ BankLoginMenu/Program.cs
@@ -30,10 +30,39 @@ namespace BankLoginMenu
             // chack username and password
             if (userName.ToLower() == "system" && password == "manager")
             {
-                Console.WriteLine("TO DO: Main menu here");
+                // declare variable to store menu choice
+                int mainMenuChoice = -1;
+                do
+                {
+                    Console.WriteLine("\n::Main menu::");
+                    Console.WriteLine("1.Customers");
+                    Console.WriteLine("2.Accounts");
+                    Console.WriteLine("3.Funds Transfer");
+                    Console.WriteLine("4.Funds Transfer Statement");
+                    Console.WriteLine("5.Account Statement");
+                    Console.WriteLine("0.Exit");
+
+                    Console.Write("Enter choice: ");
+                    mainMenuChoice = int.Parse(Console.ReadLine());
+
+                    switch (mainMenuChoice)
+                    {
+                        case 1: break;
+                        case 2: break;
+                        case 3: break;
+                        case 4: break;
+                        case 5: break;
+                        default: break;
+                    }
+                } while (mainMenuChoice != 0);
+                
+            }
+            else
+            {
+                Console.WriteLine("Invalid username or password");
             }
 
-            // about to exit
+                // about to exit
             Console.WriteLine("Thank you! Visit again.");
         }
     }
